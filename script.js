a3f596fd591ac3ec6da3b45c3dfb5f84894c0f4b
OYUGI MOURICE <165279792+oyugijr@users.noreply.github.com>
2024-05-26 16:57:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-document.addEventListener('DOMContentloaded', function() {
-  const menuToggle = document.getElementById('menu-toggle');
-  const menu = document.querySelector('.menu');
-  
-  menuToggle.addEventListener('change', function() {
-    if (menuToggle.checked) {
-        menu.style.display = 'flex';
-    }
-    else {
-      menu.style.display = 'none';
-    }
-  });
+
+      document.addEventListener("DOMContentLoaded", function() {
+    const hamburger = document.getElementById('hamburger');
+    const navLinks = document.getElementById('nav-links');
+
+    hamburger.addEventListener('click', function() {
+        navLinks.classList.toggle('active');
+    });
 });
+
+  
